bc870c867527cf83a11947c77fcf713bc69d3802
Black-Cog <blackcogdigital@gmail.com>
2015-03-22 09:55:52 +0100

    Added installer

--- bin/launchCoalMaya.py
+++ bin/launchCoalMaya.py
@@ -4,7 +4,7 @@ import sys
 if 'launchCoal' in sys.modules:
 	del sys.modules['launchCoal']
 else:
-	sys.path.append('F:/dev')
-	sys.path.append('F:/dev/Coal/bin/')
+	sys.path.append('F:/software/coal_0.0.0.1dev/')
+	sys.path.append('F:/software/coal_0.0.0.1dev/Coal/bin')
 
 import launchCoal
